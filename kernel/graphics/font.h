e40cceb19a2e5a4afbaff0a4f57891a73e239ca2
itay2805 <itay2805@protonmail.com>
2019-03-21 01:38:07 +0200

    Started working on a terminal right now got the font and the function prototypes

---
+++ kernel/graphics/font.h
@@ -0,0 +1,24 @@
+#ifndef GRAPHICS_TERM_H
+#define GRAPHICS_TERM_H
+
+#include "../common/stdint.h"
+
+////////////////////////////////////////////////////////////////////////////
+// Font
+////////////////////////////////////////////////////////////////////////////
+//
+// This is the default font for the terminal.
+//
+// Currently only supports basic latin characters (first 128 
+// characters of ASCII)
+//
+////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This is a bitmap font for the os
+ * 
+ * This basically only has latin characters
+ */
+extern char font_basic[128][8];
+
+#endif
