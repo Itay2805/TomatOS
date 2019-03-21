5c88e05de86e01e3a75ed9ddeacc4c9d2512a614
Itay Almog <itay2805@protonmail.com>
2019-03-21 02:44:05 +0200

    Fixed multibot header to support graphics mode and identity mapping first 4GB

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,7 +2,15 @@
 
 #include "graphics/term.h"
 
+extern uint64_t* boot_pdpe;
+
+
 void kernel_main(multiboot_info_t* info) {
+    // first of all map the first 4GB
+    for(int i = 0; i < 4; i++) {
+        boot_pdpe[i] = ((uint64_t)((0x7FFFFFFFC0000000) & (i * (1 << 20)))) | (1 << 1) | (1 << 0) | (1 << 7);
+    }
+
     term_init(info);
     term_clear();
 
--- kernel/multiboot.asm
+++ kernel/multiboot.asm
@@ -15,7 +15,7 @@ BITS 32
 ; Header values
 ; ------------------------------------
 MAGIC equ 0x1badb002
-FLAGS equ ((1 << 0) | (1 << 1))
+FLAGS equ ((1 << 0) | (1 << 1) | (1 << 2))
 CHECKSUM equ -(MAGIC + FLAGS)
 
 ; ------------------------------------
@@ -27,6 +27,20 @@ multiboot_header:
 dd MAGIC
 dd FLAGS
 dd CHECKSUM
+
+; these are the address fields, we just ignore these
+dd 0 ; header_addr
+dd 0 ; load_addr
+dd 0 ; load_end_addr
+dd 0 ; bss_end_addr
+dd 0 ; entry_addr
+
+; these are the graphics fields
+dd 0    ; video mode
+dd 1024 ; width
+dd 768  ; height
+dd 32   ; depth (bpp)
+
 ; ====================================
 ;   Multiboot entry
 ; ====================================
@@ -37,24 +51,20 @@ multiboot_main:
 
     ; TODO: Check that we were loaded from multiboot
 
-    ; push the parameters to the
-    ; kernel main function
-    push ebx
-
     ; set the first pml4 entry
     ; sets the address of the pdpe
     ; and then set some flags
      ; PRESENT | READ_WRITE
-    mov eax, pdpe
+    mov eax, boot_pdpe
     or eax, 0x3
-    mov [pml4], eax
+    mov [boot_pml4], eax
 
     ; set the pdpe entry, starts at physical 0
     ; all the way to 1GB
     ; PRESENT | READ_WRITE | LARGE_PAGE
-	mov dword [2*ecx+pdpe], 0x83
+	mov dword [boot_pdpe], 0x83
 
-    mov eax, pml4
+    mov eax, boot_pml4
     mov cr3, eax
 
     ; Enable PAE-paging
@@ -80,10 +90,8 @@ multiboot_main:
 BITS 64
     extern kernel_main
     multiboot_64bit:
-        xor rbp, rbp
-        pop ebp
         mov rsp, kernel_stack
-        push rbp
+        mov rdi, rbx
         call kernel_main
         add rsp, 4
 
@@ -149,11 +157,13 @@ gdt64:
 SECTION .bss
 
 alignb 4096
-pml4:
+global boot_pml4
+boot_pml4:
 resq 512
 
 alignb 4096
-pdpe:
+global boot_pdpe
+boot_pdpe:
 resq 512
 
 ; ====================================
