8eeb46bb5fcf9c32a2fd63781428f0b050e62a03
itay2805 <itay2805@protonmail.com>
2019-05-11 18:34:18 +0300

    Changed so when using bufs they will delegate the filename and line

--- kernel/common/buf.h
+++ kernel/common/buf.h
@@ -38,9 +38,9 @@ typedef struct BufHdr {
 #define buf_resize(b, s) ((b) ? buf__hdr(b)->len = s : 0)
 
 #define buf_free(b) ((b) ? (kfree(buf__hdr(b)), (b) = NULL) : 0)
-#define buf_fit(b, n) ((n) <= buf_cap(b) ? 0 : ((b) = buf__grow((b), (n), sizeof(*(b)))))
+#define buf_fit(b, n) ((n) <= buf_cap(b) ? 0 : ((b) = buf__grow((b), (n), sizeof(*(b)), __FILENAME__, __LINE__)))
 #define buf_push(b, ...) (buf_fit((b), 1 + buf_len(b)), (b)[buf__hdr(b)->len++] = (__VA_ARGS__))
 #define buf_clear(b) ((b) ? buf__hdr(b)->len = 0 : 0)
 
-void *buf__grow(const void *buf, size_t new_len, size_t elem_size);
+void *buf__grow(const void *buf, size_t new_len, size_t elem_size, const char* filename, int line);
 #endif
