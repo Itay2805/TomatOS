fa2943238b2e297900a6695c084cf11f837149e3
Itay Almog <itay2805@protonmail.com>
2019-06-10 01:38:19 +0300

    Fixed a few problems in the irq handlers

--- src/interrupts/irq.c
+++ src/interrupts/irq.c
@@ -33,30 +33,30 @@ extern void irq_23();
 
 void irq_init() {
     log_debug("Setting interrupt stubs");
-    idt_set_entry(INTERRUPT_IRQ_BASE + 0, irq_0, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 1, irq_1, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 2, irq_2, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 3, irq_3, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 4, irq_4, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 5, irq_5, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 6, irq_6, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 7, irq_7, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 8, irq_8, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 9, irq_9, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 10, irq_10, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 11, irq_11, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 12, irq_12, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 13, irq_13, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 14, irq_14, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 15, irq_15, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 16, irq_16, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 17, irq_17, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 18, irq_18, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 19, irq_19, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 20, irq_20, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 21, irq_21, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 22, irq_22, IDT_INTERRUPT_GATE, 0);
-    idt_set_entry(INTERRUPT_IRQ_BASE + 23, irq_23, IDT_INTERRUPT_GATE, 0);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 0, irq_0, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 1, irq_1, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 2, irq_2, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 3, irq_3, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 4, irq_4, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 5, irq_5, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 6, irq_6, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 7, irq_7, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 8, irq_8, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 9, irq_9, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 10, irq_10, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 11, irq_11, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 12, irq_12, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 13, irq_13, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 14, irq_14, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 15, irq_15, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 16, irq_16, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 17, irq_17, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 18, irq_18, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 19, irq_19, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 20, irq_20, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 21, irq_21, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 22, irq_22, IDT_INTERRUPT_GATE, 1);
+    idt_set_entry(INTERRUPT_IRQ_BASE + 23, irq_23, IDT_INTERRUPT_GATE, 1);
 }
 
 void irq_common(registers_t regs) {
