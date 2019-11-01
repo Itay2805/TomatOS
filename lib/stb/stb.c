3940b96851b74e16ad75bda1f02e20329cd084f7
Itay Almog <itay2805@protonmail.com>
2019-11-01 12:57:38 +0200

    removed lai for now

--- lib/stb/stb.c
+++
@@ -1,8 +0,0 @@
-#include <memory/mm.h>
-
-#define STB_DS_IMPLEMENTATION
-#include "stb_ds.h"
-
-#define STB_SPRINTF_IMPLEMENTATION
-#define STB_SPRINTF_NOFLOAT
-#include "stb_sprintf.h"
--- src/smp/smp.h
+++
@@ -1,8 +0,0 @@
-#ifndef TOMATKERNEL_MP_H
-#define TOMATKERNEL_MP_H
-
-#include <error.h>
-
-error_t smp_init();
-
-#endif //TOMATKERNEL_MP_H
