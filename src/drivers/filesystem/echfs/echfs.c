113c5333dc0be24af13c2c18f6b13b6ea733ff1d
Itay Almog <itay2805@protonmail.com>
2019-11-16 15:40:32 +0200

    Some more work on echfs

--- src/drivers/filesystem/echfs/echfs.c
+++ src/drivers/filesystem/echfs/echfs.c
@@ -3,8 +3,19 @@
 #include "echfs.h"
 #include "echfs_spec.h"
 
-static void* Echfs_ctor(void* _self, va_list ap) {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Echfs filesystem implementation
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void* Echfs_filesystem_ctor(void* _self, va_list ap) {
     echfs_filesystem_t* self = super_ctor(EchfsFilesystem(), _self, ap);
+
+    echfs_header_t header;
+    ASSERT(!IS_ERROR(partition_read_block(self->super.parent, 0, &header, sizeof(echfs_header_t))));
+
+    self->allocation_table_lba = 16;
+    self->main_directory_lba = self->allocation_table_lba + ((header.total_block_count * sizeof(uint64_t)) / header.bytes_per_block);
+
     return self;
 }
 
@@ -13,11 +24,35 @@ const void* EchfsFilesystem() {
     if(unlikely(class == NULL)) {
         class = new(FileSystemClass(),
                 "EchfsFilesystem", FileSystem(), sizeof(echfs_filesystem_t),
-                ctor, Echfs_ctor,
+                ctor, Echfs_filesystem_ctor,
                 0);
     }
     return class;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Echfs file implementation
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void* Echfs_file_ctor(void* _self, va_list ap) {
+    echfs_file_t* self = super_ctor(EchfsFile(), _self, ap);
+    return self;
+}
+
+const void* EchfsFile() {
+    static const void* class = NULL;
+    if(unlikely(class == NULL)) {
+        class = new(FileClass(),
+                "EchfsFile", File(), sizeof(echfs_file_t),
+                ctor, Echfs_file_ctor,
+                0);
+    }
+    return class;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Echfs functions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 bool check_echfs(partition_t* partition) {
     bool valid = false;
--- src/drivers/filesystem/echfs/echfs.h
+++ src/drivers/filesystem/echfs/echfs.h
@@ -6,9 +6,20 @@
 
 typedef struct echfs_filesystem {
     filesystem_t super;
+
+    uint64_t allocation_table_lba;
+    uint64_t main_directory_lba;
 } echfs_filesystem_t;
 
+typedef struct echfs_file {
+    file_t super;
+
+
+} echfs_file_t;
+
 const void* EchfsFilesystem();
+
+const void* EchfsFile();
 
 bool check_echfs(partition_t* partition);
 
